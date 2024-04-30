76eaa876690f5d040ca8e0da0e46357e8cdc2104
yuya-taka <167413446+yuya-taka@users.noreply.github.com>
2024-04-30 15:54:35 +0900

    JSでレスポンスを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,3 +1,17 @@
+const buildHTML = (XHR) => {
+  const item = XHR.response.post;
+  const html = `
+    <div class="post">
+      <div class="post-date">
+        投稿日時：${item.created_at}
+      </div>
+      <div class="post-content">
+        ${item.content}
+      </div>
+    </div>`;
+  return html;
+};
+
 function post (){
   //リクエストを送信する処理
   // console.log("イベント発火");
@@ -10,6 +24,24 @@ function post (){
     XHR.open("POST", "/posts", true);
     XHR.responseType = "json";
     XHR.send(formData);
+    XHR.onload = () => {
+
+      // 通信に失敗した場合の処理
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      };
+
+      const list = document.getElementById("list");
+      const formText = document.getElementById("content");
+      // console.log(formText.value);
+      // console.log(XHR.response);
+
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
+
+
+      formText.value = "";
+    };
   });
   
  };
